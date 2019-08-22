27c2a36826a228094d11eb931307a6974e931f87
Aniket Satarkar <aniketsatarkar9@gmail.com>
2019-08-22 13:50:10 +0530

    added a modal for saving and updating a contact in home page.

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <b-row class="m-5">
-    <b-col cols="12">
+    <b-col cols="6">
+      <h3>Contacts</h3>
+    </b-col>
+    <b-col col-6 class="d-flex justify-content-end">
+      <b-button size="sm" class="mr-1 ml-1" variant="info" @click="add">Add</b-button>
+      <b-button size="sm" class="mr-1 ml-1" v-if="this.isRowSelected" variant="success" @click="edit">Edit</b-button>
+      <b-button size="sm" class="ml-1" v-if="this.isRowSelected" variant="danger" @click="archive">Delete</b-button>
+    </b-col>
+    <b-col cols="12" class="mt-3">
       <b-table
         striped
         hover
         selectable
+        id="contacts-table"
+        :per-page="pagination.perpage"
+        :current-page="pagination.page"
         select-mode="single"
         selected-variant="success"
         :items="contacts"
@@ -12,18 +23,54 @@
         @row-selected="contactSelected">
       </b-table>
     </b-col>
-    <b-col cols="12" class="align-middle">
-      <b-button-group>
-        <b-button variant="info">Add</b-button>
-        <b-button v-if="this.isRowSelected" variant="success">Edit</b-button>
-        <b-button v-if="this.isRowSelected" variant="danger">Delete</b-button>
-      </b-button-group>
+    <b-col col-12 class="d-flex justify-content-center">
+      <b-pagination
+        v-model="pagination.page"
+        :total-rows="pagination.total"
+        :per-page="pagination.perpage"
+        aria-controls="contacts-table">
+      </b-pagination>
     </b-col>
+
+    <!-- modal -->
+    <b-modal ref="modal" ok-title="Save" @ok="save">
+      <b-form>
+        <b-form-group>
+          <b-row>
+            <b-col>
+              <b-input v-model="contact.firstName" placeholder="First Name" type="text"></b-input>
+            </b-col>
+            <b-col>
+              <b-input v-model="contact.lastName" placeholder="Last Name" type="text"></b-input>
+            </b-col>
+          </b-row>
+        </b-form-group>
+        <b-form-group>
+          <b-row>
+            <b-col>
+              <b-input v-model="contact.phone" placeholder="Phone" type="text">
+              </b-input>
+            </b-col>
+            <b-col>
+              <b-input v-model="contact.secondaryPhone" placeholder="Secondary Phone" type="text">
+              </b-input>
+            </b-col>
+          </b-row>
+        </b-form-group>
+        <b-form-group>
+          <b-row>
+            <b-col>
+              <b-input v-model="contact.email" type="email" placeholder="Email"></b-input>
+            </b-col>
+          </b-row>
+        </b-form-group>
+      </b-form>
+    </b-modal>
+
   </b-row>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
@@ -42,11 +89,24 @@ export default {
         },
         {
           key: 'phone',
-          sortable: true
+          sortable: false
         }
       ],
       contacts: [],
-      isRowSelected : false
+      selected : null,
+      isRowSelected : false,
+      pagination :{
+        page : 1,
+        perpage: 5,
+        total: 0
+      },
+      contact: {
+        firstName: '',
+        lastName: '',
+        phone: '',
+        secondaryPhone: '',
+        email: ''
+      }
     };
   },
   methods: {
@@ -69,14 +129,15 @@ export default {
             lastName: value.lastName,
             email : value.email,
             phone: value.phone,
-            actions: '<span>something!</span>'
+            secondaryPhone: value.secondaryPhone
           });
         });
         this.contacts = filteredContacts;
+        this.pagination.total = contacts.length;
       })
       .catch((res) => {
         // ...
-      });
+      })
     },
     contactSelected(row) {
       row = row[0];
@@ -85,8 +146,89 @@ export default {
         this.isRowSelected = false;
       }
       else {
+        this.selected = row;
         this.isRowSelected = true;
       }
+    },
+    add() {
+      this.contact = {};
+      this.$refs.modal.title = 'Add Contact';
+      this.$refs.modal.show();
+    },
+    edit() {
+      this.contact = this.selected;
+      this.$refs.modal.title = "Edit Contact";
+      this.$refs.modal.show();
+    },
+    archive() {
+      this.$bvModal.msgBoxConfirm('Please confirm that you want to delete everything.', {
+        title: 'Please Confirm',
+        size: 'sm',
+        buttonSize: 'sm',
+        okVariant: 'danger',
+        okTitle: 'YES',
+        cancelTitle: 'NO',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      })
+      .then(value => {
+        if(value) {
+          this.$axios({
+            url: 'http://laravel.local/api/contact/delete',
+            data: {
+              id: this.selected.id
+            },
+            method: 'post',
+            headers: {
+              Authorization: 'Bearer ' + this.$cookies.get('token')
+            }
+          })
+          .then(res => {
+            if(res.status === 200) {
+              this.getContacts();
+            }
+          })
+          .catch(res => {
+            console.log("ERROR : " + JSON.stringify(res));
+          });
+        }
+      })
+      .catch(err => {
+        console.log("ERROR : " + JSON.stringify(err));
+      })
+    },
+    save() {
+      const isEdit = (this.contact.id !== undefined && this.contact.id !== null);
+      const params = {
+        method: 'POST',
+        data: this.contact,
+        headers: {
+          Authorization: 'Bearer ' + this.$cookies.get('token')
+        }
+      };
+
+      params.url = (isEdit)
+        ? 'http://laravel.local/api/contact/update'
+        : 'http://laravel.local/api/contact/store';
+
+      this.$axios(params)
+        .then(res => {
+          if(res.status === 200) {
+            this.getContacts();
+            const msg = (isEdit)
+              ? 'Contact Updated Succssfuly!'
+              : 'Contact Added Succssfuly!';
+            this.$bvToast.toast(msg, {
+              title: 'Success!',
+              autoHideDelay: 3000,
+              variant: 'success'
+            })
+          }
+        })
+        .catch(res => {
+          console.log("CATCH : " + JSON.stringify(res));
+        });
     }
   },
   created() {
